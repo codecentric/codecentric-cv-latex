49cc619a68c3b00a670165f7fd8a6f06ec61c87c
Robert Hostlowsky <robert.hostlowsky@codecentric.de>
2015-10-26 17:04:56 +0100

    avoid sudo

--- docker-build.sh
+++ docker-build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo docker build -t codecentric-cv-latex .
-sudo docker run --rm -v $(pwd):/opt codecentric-cv-latex
+docker build -t codecentric-cv-latex .
+docker run --rm -v $(pwd):/opt codecentric-cv-latex
